492a866af6b049fdf80440f4235b5cc75163971c
jatac <jan.atac@vialink.fr>
2017-08-25 18:03:16 +0200

    remove copyright which doesn't apply

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,27 +1,23 @@
 //
 // MIT License
-// Copyright (c) 2016 iAchieved.it
 //
 import MySQL
-import Foundation
-import Jay
-
 
 print("Hello, world!")
 
-print("Let's add some records to a database")
+var host = "localhost"
+var user = "root"
+var password = ""
+var database = "local_ecautions"
 
 var mysql:Database
 do {
-    mysql       = try Database(host:"localhost",
-                    user:"root",
-                    password:"",
-                    database:"local_ecautions")
+    mysql       = try Database(host:host,
+                    user:user,
+                    password:password,
+                    database:database)
     let results = try mysql.execute("SELECT @@version")
     print("VERSION : \(results)")
-    
-    let selectResults = try mysql.execute("SELECT pk_tarif_facturation_id, fk_banque_id, fk_entite_id,type_utilisateur, mode_facturation FROM local_ecautions.tarif_facturation  WHERE fk_banque_id = 7 AND fk_entite_id = 2689 AND type_utilisateur = 1 AND mode_facturation = 0")
-    print("SELECT RESULTS : \(selectResults)")
     
 } catch {
   print("Unable to connect to MySQL:  \(error)")
